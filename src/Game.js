366e68614a174597f4a8b20aec776c586fbd37c0
kupehtci <daniel.laplana.gimeno@gmail.com>
2023-01-17 10:26:06 +0100

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,90 @@
+//____________________________________________________________________
+//MAIN VARS
+
+var canvas; 
+var ctx; 
+var screenWidth = 640; 
+var screenHeight = 500; 
+
+//____________________________________________________________________
+//CREATE CANVAS IN SCREEN
+canvas = document.createElement("canvas"); 
+ctx = canvas.getContext("2d"); 
+
+canvas.width = screenWidth; 
+canvas.height = screenHeight;
+
+document.body.appendChild(canvas);      //Create the canvas in the HTML document
+
+//___________________________________________________________________
+//EMPTY
+
+
+//____________________________________________________________________
+//START - Load things the start of the game
+function Start(){
+	console.log("Start Game"); 
+}
+//____________________________________________________________________
+//RESET THE GAME
+function Reset(){
+	console.log("Reset Game"); 
+}
+
+//____________________________________________________________________
+//RESTART THE GAME
+function Restart(){
+
+} 
+
+//____________________________________________________________________
+//RENDER THE GAME
+function Render(){
+	
+}
+
+function Update(keysDownArray, modifier){
+	
+}
+
+
+//____________________________________________________________________
+//CHECK THE INPUTS
+var keysDown = {}; 
+addEventListener("keydown",function(e){
+	keysDown[e.keyCode] = true;
+}, false); 
+addEventListener("keyup",function(e){
+	delete keysDown[e.keyCode];}, 
+	false);  
+
+//____________________________________________________________________
+// THE MAIN GAME LOOP
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+	Update(keysDown, delta / 1000);
+	Render();
+
+	//Update time vars
+	then = now;
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+//____________________________________________________________________
+//Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+//TIME VARS
+var then = Date.now(); 
+var tick = 0; 
+
+//MAIN GAME LOOP
+Reset(); 
+Start(); 
+
+main();		//Start the main loop of the game 
---
+++ src/Window.js
@@ -0,0 +1,11 @@
+class Window{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+
+        //create a canvas element sprite
+        
+    }
+}
