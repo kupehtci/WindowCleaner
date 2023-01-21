371c5e80e553e8f2e17f1ec51aaeb387a2cb6123
kupehtci <daniel.laplana.gimeno@gmail.com>
2023-01-21 12:34:25 +0100

    Added font, try to addapt into the proyect

--- src/Manager.js
+++ src/Manager.js
@@ -2,11 +2,6 @@
 
 class Manager{
     constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.width = 0;
-        this.height = 0;
-
 
 
         this.money = 321;
@@ -14,28 +9,19 @@ class Manager{
         this.totalMoneyEarned = 0;
 
 
-        // this.imageReady = false;
-        // this.sprite = null;
-        // this.scale = 0.3;
-
-        // let image = new Image();
-        // image.onload = () => {
-        //     this.height = image.height * this.scale;
-        //     this.width = image.width * this.scale;
-        //     this.sprite = image;
-        //     this.imageReady = true;
-        // }
-        // image.src = "./assets/hud.png";
+        
     }
 
     Render(){
-        // if(this.imageReady){
-        //     ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
-        // }
-
+        //Render the money text
         ctx.fillStyle = "black";
         ctx.font = "30px Arial";
         ctx.fillText(this.moneyText, 10, 50);
+
+        //Render the total money earned
+        ctx.fillStyle = "black";
+        ctx.font = "30px Arial";
+        ctx.fillText("Total Money Earned: " + this.totalMoneyEarned, 10, 100);
     }
 
     Update(){
